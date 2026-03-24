9adde12ff20a8491f517d7822f069e50eeebce86
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-03-24 23:09:28 +0100

    Add OpenAI request logic and Fake AI Result

--- analyzer.py
+++ analyzer.py
@@ -4,22 +4,29 @@ from config import API_KEY
 client = OpenAI(api_key = API_KEY)
 
 def analyze_job(job):
-    prompt = f""" 
-    Analyze this job:
-
-    Title: {job['title']}
-    Company: {job['company']}
-
-    Give:
-    - Short summary
-    - Required skills
-    - Level (junior/mid/senior)
+    # Fake AI Function Result 
+    return f"""
+    Summary: This is a {job['title']} role at {job['company']}.
+    Skills: Skills
+    Level: Junior/Mid/Senior
     """
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "user", "content": prompt}
-        ]
-    )
+    # OpenAI Logic
+    # prompt = f""" 
+    # Analyze this job:
 
-    return response.choices[0].message.content
+    # Title: {job['title']}
+    # Company: {job['company']}
+
+    # Give:
+    # - Short summary
+    # - Required skills
+    # - Level (junior/mid/senior)
+    # """
+    # response = client.chat.completions.create(
+    #     model="gpt-4o-mini",
+    #     messages=[
+    #         {"role": "user", "content": prompt}
+    #     ]
+    # )
+    #
+    # return response.choices[0].message.content
