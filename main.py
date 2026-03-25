c4d3d80655b7d078693032edf42d45c2f1e34bab
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-03-25 02:22:24 +0100

    Edit code cleaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from analyzer import analyze_job
+from analyzer import analyze_jobAI, analyze_jobFake
 
 #Loads Job List
 def load_jobs():
@@ -34,8 +34,8 @@ def main():
         return
     while True:
         print("\n1.Show jobs")
-        print("2.Analyze jobs")
-        print("3.Analyze specific job")
+        print("2.Analyze jobs (AI)")
+        print("3.Analyze specific job (AI)")
         print("4.Clear analysis file")
         print("5.Exit")
         choice = input("\nEnter option: ")
@@ -48,10 +48,11 @@ def main():
         #Analyze jobs (all)
         elif choice == "2":
             for i, job in enumerate(jobs[:5], 1):
-                print(f"\n{i}. {job['title']} - {job['company']}")
-
-                result = analyze_job(job)
+                result = analyze_jobAI(job)
+                if result == None:
+                    break
                 save_analysis(result)
+                print(f"\n{i}. {job['title']} - {job['company']}")
                 print(result) 
             continue
 
@@ -67,10 +68,12 @@ def main():
             if 1 <= job_choice <= len(jobs): #Check if entered index exists
                 for i, job in enumerate(jobs, 1):
                     if i == job_choice:
-                        print(f"{i}. {job['title']} - {job['company']}")
-                        result = analyze_job(job)
-                        print(result)
+                        result = analyze_jobAI(job)
+                        if result == None:
+                            break                       
                         save_analysis(result)
+                        print(f"{i}. {job['title']} - {job['company']}")
+                        print(result)
             else:
                 print("\nIndex does not exist.")
 
